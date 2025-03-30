ea89d01c937384dfe7380d717fcd464906ab6df4
Ezio-droid <69637069+Ezio-droid@users.noreply.github.com>
2025-03-30 13:21:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('💨 CO2 BCA App')
 
 st.info('This app help in analyzing CO2 breakthrough curves')
+
+df = pd.read_csv('https://raw.githubusercontent.com/Ezio-droid/data/refs/heads/main/combine_data_blank_processed_cp.dat')
+df
