0355dcfe097e63b6eb8fb6002f81f03432f8fa8d
Ezio-droid <69637069+Ezio-droid@users.noreply.github.com>
2025-03-30 16:16:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,18 +9,18 @@ st.info('This app help in analyzing CO2 breakthrough curves')
 
 with st.expander('Data'):
   st.write('**Raw data**')
-  df = pd.read_csv('https://raw.githubusercontent.com/Ezio-droid/data/refs/heads/main/combine_data_blank_processed_cp.dat')
-  df.columns = ['Time', 'CO2']
+  df = pd.read_csv('https://raw.githubusercontent.com/Ezio-droid/data/refs/heads/main/combine_data_r10.dat', delim_whitespace=True, header=None)
+  df['time (sec)'] = df[0] - df.iloc[0,0]
   df
   
-with st.expander('Data visualization'):
-  _lock = RLock()
-  x = df.iloc[:,0]
-  y = df.iloc[:,1]
-  with _lock:
-    fig, ax = plt.subplots()
-    ax.scatter(x,y)
-    st.pyplot(fig)
+#with st.expander('Data visualization'):
+#  _lock = RLock()
+#  x = df.iloc[:,0]
+#  y = df.iloc[:,1]
+#  with _lock:
+#    fig, ax = plt.subplots()
+#    ax.scatter(x,y)
+#    st.pyplot(fig)
 
 #Parameters
 with st.sidebar:
